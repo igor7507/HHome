c89d340ce21e34fd98d3d13ba0321d2602d18b86
igor7507 <igor7507@gmail.com>
2017-03-27 11:19:48 +0300

    Add files via upload

---
+++ vybor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication36
+{
+    class Program
+    {
+        static int[] ViborSort(int[] mas)
+        {
+            
+            for (int i = 0; i < mas.Length - 1; i++)
+            {
+                //пошук мінімального числа
+                int min=i;
+                for (int j = i + 1; j < mas.Length; j++)
+                {
+                    if (mas[j] < mas[min])
+                    {
+                        min = j;
+                    }
+                }
+                //обмін елементів
+                int temp = mas[min];
+                mas[min] = mas[i];
+                mas[i] = temp;
+            }
+
+            return mas;
+        }       
+        static void Main(string[] args)
+        {
+            int n = 20;
+            int[] mas = new int[n]; //введення масиву
+            Random rand = new Random();
+            Console.WriteLine("Згенерований масив");
+            
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = rand.Next(1, 100);
+                Console.WriteLine("" + mas[i]); //заповнення випадковими числами
+            }
+            ChooseSort(mas);
+            Console.WriteLine("Відсортований масив:");
+            
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine( mas[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+  
---
+++ бкульки.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+        
+namespace ConsoleApplication35
+{
+        class Program
+    {
+            static int[] BubbleSort(int[] mas)  //vvedennya function sortyvannya "Bubbles"
+            {
+                int temp;
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (mas[i] > mas[j])
+                        {
+                            temp = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = temp;
+                        }
+                    }
+                }
+                return mas;
+            }
+        static void Main(string[] args)
+        {
+            int n = 20;
+            int[] mas = new int[n]; //vvedennya massivu
+            Random rand = new Random();
+            Console.WriteLine("Zgenerovanniy massiv");
+            for (int i = 0; i < n; i++)
+            {
+                mas[i] = rand.Next(1, 100);
+                Console.WriteLine("" + mas[i]); //zapovnennya rand 4islami
+            }
+            BubbleSort(mas);
+            Console.WriteLine("Vivedennya sorted massive:");
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.WriteLine(mas[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
